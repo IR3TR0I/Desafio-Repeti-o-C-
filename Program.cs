3aa7ba891726997f6086097b96a9da68a5d0c77a
Ruan <ruangustavo50711@gmail.com>
2020-11-18 17:05:04 -0300

    Desafio Finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Desafio_Repetição_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int num;
+
+            int x=1, y=0, z=0;
+            num =int.Parse(Console.ReadLine());
+
+            for(int i=0; i < num; i++){
+                z= x + y;
+                Console.Write(z + ",");
+                y=x;
+                x=z;
+            }Console.ReadLine();
+        }
+    }
+}
